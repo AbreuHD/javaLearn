63b0166fb5b5687a0cb72c6ecf2956085fe40fdf
Jefferson <abreumartinezjefferson@gmail.com>
2021-04-29 22:57:37 -0400

    idk bro 2d :D

---
+++ src/enterData.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class enterData {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, n3;
+
+        System.out.println("Introduce tus numero");
+        n1 = sc.nextInt();
+
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,8 +1,8 @@
 public class test {
     public static void main(String[] args){
         String saludo = "hola world";
-        System.out.println(saludo);
+        System.out.println(saludo.toUpperCase());
         int n1 = 0;
-        
+
     }
 }
