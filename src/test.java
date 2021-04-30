ee7317fc97c075e2105f5001b886728455f378bb
Jefferson <abreumartinezjefferson@gmail.com>
2021-04-29 23:20:04 -0400

    4to c

--- src/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args){
-        String saludo = "hola world";
-        System.out.println(saludo.toUpperCase());
-        int n1 = 0;
-
-    }
-}
---
+++ src/tiposDatos.java
@@ -0,0 +1,19 @@
+public class tiposDatos {
+    public static void main(String[] args){
+        //Strings
+        String name = "Jefferson";
+        String lastName;
+        lastName = "Abreu";
+        System.out.println("Mi nombre es " + name + " " + lastName);
+
+        //Datos
+        int age = 18;
+        int bornDay;
+        bornDay = 27;
+
+        //booleanos
+        if(age >= 18){
+            System.out.println("Mi nombre es " + name + " " + lastName + " y naciste el dia " + bornDay);
+        }
+    }
+}
