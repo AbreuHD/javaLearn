03732eb934e01b1a9f4802f3d3cc28ac743a495f
Jefferson <abreumartinezjefferson@gmail.com>
2021-05-03 11:32:33 -0400

    46

---
+++ src/OperadorInstanceOf.java
@@ -0,0 +1,10 @@
+public class OperadorInstanceOf {
+    public static void main(String[] args){
+
+        String text0 = "Idk broz dizculpa";
+
+        Integer num64 = 7;
+
+        System.out.println(text0 instanceof String? "Cy" : "nelson");
+    }
+}
