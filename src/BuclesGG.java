da4ebb19a0848ee61da39fda7eb83af3a037297e
Jefferson <abreumartinezjefferson@gmail.com>
2021-05-05 07:47:20 -0400

    58

--- src/BuclesGG.java
+++ src/BuclesGG.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 public class BuclesGG {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int cant = sc.nextInt();
+        System.out.println("cuantos numeros piensas agregar?");
+        int[] ggbro = new int[sc.nextInt()];
 
-        System.out.println("Añade el numero");
-
-        for (int i = 0 ; i < cant ; i++ ){
-            System.out.println("i = " + i);
+        for(int i = 0 ; i < ggbro.length ; i++){
+            System.out.println("añade el " + (i+1) + " numero");
+            ggbro[i] = sc.nextInt();
+            System.out.println("ggbro[i] = " + ggbro[i]);
         }
-        
     }
 }
