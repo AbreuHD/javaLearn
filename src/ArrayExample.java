ca20ea2976d7c6e0acadd94f56274af9482bd886
Jefferson <abreumartinezjefferson@gmail.com>
2021-05-18 20:38:59 -0400

    No recuerdo

---
+++ src/ArrayExample.java
@@ -0,0 +1,10 @@
+public class ArrayExample {
+    public static void main(String[] args){
+
+        int[] numerosEnteros = {1, 2, 3, 4};
+
+        for(int i: numerosEnteros){
+            System.out.println("numerosEnteros = " + i);
+        }
+    }
+}
